087b029b0dca12859545184660f0bd3a559c4cb9
kabambaarchippe78-maker <kabambaarchippe78@gmail.com>
2026-03-08 12:40:06 +0100

    Correction ligne 13 a 19 CORS

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headres=["*"]
 )
 Base.metadata.create_all(bind=engine)
 @app.get("/")
